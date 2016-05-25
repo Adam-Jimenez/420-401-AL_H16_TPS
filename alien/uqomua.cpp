0145a4222f4015cb3459b7ba205a5aa9e3f9e192
kev <carosilvak@outlook.com>
2016-05-24 23:50:17 -0400

    commence uqoma

--- alien/uqomua.cpp
+++ alien/uqomua.cpp
@@ -11,22 +11,31 @@ AlienUqomua::AlienUqomua() : Alien(Uqomua)
 Alien::Attack AlienUqomua::queryAttack(Color   alienColor,
                                        Species alienSpecies)
 {
+    if(alienColor==Red || alienColor==Green){
+        if(alienSpecies!=Uqomua){
+            return Acid;
+        }
+    } else if(alienColor==Yellow || alienColor==Gray){
+        return Plasma;
+    } else return Fungus;
+
     return Forfeit;
 }
 
+// faut faire verifs? (si mange, dort, etc)
 Alien::Move AlienUqomua::queryMove()
 {
-    return None;
+    return (m_move=!m_move)? Right : Down ;
 }
 
 bool AlienUqomua::queryEat()
 {
-    return false;
+    return true;
 }
 
 Alien::Color AlienUqomua::queryColor()
 {
-    return Red;
+    return Color() == Red? Green : Red;
 }
 
 Alien::Species AlienUqomua::querySpecies()
